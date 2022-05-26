5a3c4c3099143c644183ac16fd0a2bd6987d9e37
Ricard Boix <ricard.boix@allianz.es>
2022-05-26 12:12:55 +0200

    Subida ejercicio2 y ejercicio3

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,18 @@
+package ejercicio2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Nombre\tApellido\tEmail          \t\tteléfono");
+		System.out.println ("======\t=========\t==============\t\t========");
+		System.out.println ("Ricard\tBoix Planas\trboixp@gmail.com\t657868060");
+		System.out.println ("Pepe\tLopez Lopez\tpepelo@gmail.com\t600000000");
+		System.out.println ("Luís\tAlonso Alonso\tlalonso@gmail.com\t610000000");
+		System.out.println ("Carles\tPerez Perez\tcarles.perez@gmail.com\t625607000");
+		System.out.println ("Pere\tMartí Martí\tperem@gmail.com\t\t657000099");
+		System.out.println ("Marc\tGilabert Bosch\tmgilabert@gmail.com\t657008900");	
+
+	}
+
+}
---
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,17 @@
+package ejercicio3;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("tecla de escape\t\tsignificado");
+	//	System.out.println ("\n");
+		System.out.println ("\\n\t\t\tSignifica nueva línea");
+		System.out.println ("\\t\t\t\tSignifica tab de espacio");
+		System.out.println ("\\\"\t\t\tes para poner \" (comillas dobles) dentro del texto por ejemplo \"Belencita\"");
+		System.out.println ("\\\\\t\t\tSe utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\ ");
+		System.out.println ("\\'\t\t\tSe utiliza para las '(comilla simple) para escribir por ejemplo 'Princesita' ");
+
+	}
+
+}
